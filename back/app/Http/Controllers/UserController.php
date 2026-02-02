ba80a18b8142c9e93c79728a6518afabdf2741d1
M1LOS13 <milos00vitosevic@gmail.com>
2026-02-02 15:12:55 +0100

    Dodata logika za prikaz svih autora, kao i prikaz omiljenih podkasta ulogovanog korisnika

--- back/app/Http/Controllers/UserController.php
+++ back/app/Http/Controllers/UserController.php
@@ -32,6 +32,50 @@
     }
     
 
+    
+    public function mojiPodcasti(Request $request)
+    {
+        try{
+            $perPage = $request->input('per_page', 10); 
+            $user = Auth::user();
+            $podkasti = $user->mojiPodkasti()->paginate($perPage);
+            return PodcastResource::collection($podkasti);
+        }catch (\Exception $e) {
+           
+            return response()->json([
+                'message' => 'Došlo je do greške prilikom dohvatanja podkasta.',
+                'error' => $e->getMessage(),
+            ], 500);
+        }
+       
+    }
+
+
+    
+    public function getFavorites(Request $request)
+    {
+        try {
+            $user = Auth::user();
+            $query = $user->listaOmiljenihPodkasta()->newQuery();
+    
+            if ($request->filled('id_autora')) {
+                $query->whereHas('autori', function ($q) use ($request) {
+                     $q->where('users.id', $request->id_autora);
+                });
+            }
+    
+            $perPage = $request->input('per_page', 10); 
+            $omiljeniPodkasti = $query->paginate($perPage);
+    
+            return PodcastResource::collection($omiljeniPodkasti);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Došlo je do greške pri dohvatanju omiljenih podkasta.',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+    
 
      
  }
--- back/routes/api.php
+++ back/routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\PodcastController;
 use App\Http\Controllers\KategorijaController;
 use App\Http\Controllers\UserController;
 
+
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
@@ -15,7 +16,9 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('/podcasti',[PodcastController::class,'index']);
       Route::get('kategorije',[KategorijaController::class,'index']);
-       Route::get('users/autori',[UserController::class,'vratiAutore']);
+      Route::get('users/autori',[UserController::class,'vratiAutore']);
+      Route::get('/users/podcasti', [UserController::class, 'mojiPodcasti']);
+      Route::get('users/favorites/podcasti',[UserController::class,'getFavorites']);
 
 
 });
